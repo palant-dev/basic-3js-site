9823c05fa542b8dcf7ee0f4da1e2eb9bb8f5acc1
palant-dev <antonio.palomba2015@gmail.com>
2025-02-15 16:14:44 +0100

    Adds the vite basic projects with orbiting camera and controls

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.getElementById('canvas');
 
@@ -9,13 +10,13 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = 5;
 
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({ color: '#468585'});
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585'});
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshLambertMaterial({ color: '#B4B4B3'});
+const boxMaterial = new THREE.MeshLambertMaterial({ color: '#B4B4B3', emissive: '#B4B4B3'});
 
-const box = new THREE.Mesh(boxGeometry, material);
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
 scene.add(dodecahedron);
@@ -27,5 +28,30 @@ scene.add(light);
 
 const renderer = new THREE.WebGLRenderer( { canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+renderer.setPixelRatio(window.devicePixelRatio);
 
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+    box.rotation.y += 0.005;
+
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate()
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight); 
+})
